e415d2e62d5ccaac2faa14001c06d65ebb4189a0
Anderson G. A. Alves <andersongaalves1@gmail.com>
2026-06-20 15:44:24 -0300

    feat: production api

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,7 @@
+const LOCAL_API = "http://localhost:8000";
+const PROD_API = "https://mirai-hit-studio.onrender.com";
 
-
-export const API_URL = "http://localhost:8000";
+export const API_URL =
+    window.location.hostname === "localhost"
+        ? LOCAL_API
+        : PROD_API;
